01ef50e2dc97a8ecfa47bb14240432b8bd2799ea
nathaliamaimone <nathalia.pmaimone@gmail.com>
2024-10-17 08:58:02 -0300

    Multi-browser tests

--- e2e/login.spec.ts
+++ e2e/login.spec.ts
@@ -12,7 +12,7 @@ test('senha obrigatória', async ({ page }) => {
 
 test('usuário não existe', async ({ page }) => {
   await login(page,'teste', 'teste')
-  await toast(page, 'ERRO ERRO Oops! Credenciais inválidas :(')
+  await toast(page, 'Oops! Credenciais inválidas :(')
 })
 
 test('senha incorreta', async ({ page }) => {
